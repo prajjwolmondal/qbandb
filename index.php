d8f3da9f09f016f3ad02507685993bf8fd82169a
Simon Zhang <simonkhzhang@gmail.com>
2016-03-22 16:59:08 -0400

    no message

--- index.php
+++ index.php
@@ -14,8 +14,23 @@
 
 	<?php
 	    include_once 'navbar.php';
-	    include_once 'config/connection.php';
+	    //include_once 'config/connection.php';
 
+// used to connect to the database
+$host = "eu-cdbr-azure-west-d.cloudapp.net";
+$db_name = "qbandb";
+$username = "bd22e1627c7160";
+$password = "91bad711";
+
+try {
+    $con = new PDO($host,$username,$password, $db_name);
+    $con->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION, PDO::ERRMODE_WARNING );
+}
+ 
+// show error
+catch(Exception $exception){
+    echo "Connection error: " . $exception->getMessage();
+}
         /*
         if (mysqli_connect_errno())
         {
