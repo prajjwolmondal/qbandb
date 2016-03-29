5b05dd9ce2f446ffd0c213daa217a4e7e5ca2880
Jay <jayzhao94@gmail.com>
2016-03-28 22:02:21 -0400

    recommitt

---
+++ dashboard.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML>
+<html>
+  <head>
+        <title>Dashboard - Queen's BnB</title>
+        
+        <!-- Compiled and minified CSS -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/css/materialize.min.css">
+    <!-- Icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- JS animations -->
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+		<!-- Compiled and minified JavaScript -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.5/js/materialize.min.js"></script>
+    <!-- Dropdown Animations -->
+    <script>  
+      $(document).ready(function() {
+      $('select').material_select();
+      }); </script>
+   
+  
+  </head>
+
+  <body>
+
+	<?php
+     include_once 'navbar.php';
+     include_once 'config/connection.php';
+	?>
+
+
+  
+
+
+
+  </body>
+</html>
