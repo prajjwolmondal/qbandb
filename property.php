3be3801785fd5a4f03aa7f47d7c9a6600a7a036a
Prajjwol <prajj.mondal@gmail.com>
2016-03-28 21:41:36 -0400

    - TO DO: Fix property.php javascript - Added admin.js to support admin.php - Added code to implement all required functionality for admin.php

--- property.php
+++ property.php
@@ -168,7 +168,7 @@
                 die(var_dump($e));
             }
     	?>
-<!-- Register Form -->
+<!-- Rating Form -->
   <div class="row">
     <form class="col s12" action="property.php" method="post">
       <!-- Rating -->
@@ -200,24 +200,5 @@
         </button>
     </form>
   </div>
-        <script>
-            function showReply(){
-                document.getElementsByClassName("owner_reply").innerHTML = "
-                <tr><td><div class='row'>
-                <form class='col s12' action='property.php' method='post'>
-                <div class='row'>
-                <div class='input-field col s12'>
-                <textarea id='comment' length='500' class='materialize-textarea' name='user_comment'></textarea>
-                <label for='comments'>Your Comment (Optional)</label>
-                </div>
-                </div>
-                <button class='btn waves-effect waves-light' type='submit' name='action'>Reply
-                <i class='material-icons right'>trending_flat</i>
-                </button>
-                </form>
-                </div></td></tr>";
-            }
-        </script>
-
 </body>
 </html>
